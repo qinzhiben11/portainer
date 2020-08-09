1423da90a6e94c72a1598e981620b50719cfb8f5
Itsconquest <william.conquest@portainer.io>
2020-08-09 09:38:47 +0000

    feat(app): introduce new env vars ui
    feat(app): introduce new env vars ui
    
    feat(UX): WIP new env variables UI
    
    feat(UX): update button and placeholder
    
    feat(UX): mention .env file in message
    
    feat(UX): allow add/remove value & load correctly
    
    feat(UX): restrict filesize to 1MB
    
    feat(UX): vertical align error message
    
    feat(UX): fill UI from file & when switching modes
    
    feat(UX): strip un-needed newline character
    
    feat(UX): introduce component to other views
    
    feat(UX): fix title alignment
    
    feat(UX): only populate editor on mode switch when key exists
    
    feat(UX): prevent trimming of whitespace on values
    
    feat(UX): change editor to async
    
    feat(UX): add message describing use
    
    feat(UX): Refactor variable text to editorText
    
    refactor(app): rename env vars controller
    
    refactor(app): move env var explanation to parent
    
    refactor(app): order env var panels
    
    refactor(app): move simple env vars mode to component
    
    refactor(app): parse env vars
    
    refactor(app): move styles to css
    
    refactor(app): rename functions
    
    refactor(container): parse env vars
    
    refactor(env-vars): move utils to helper module
    
    refactor(env-vars): use util function for parse dot env file
    
    fix(env-vars): ignore comments
    
    refactor(services): use env vars utils
    
    refactor(env-vars): rename files
    
    refactor(env-panel): use utils
    
    style(stack): revert EnvContent to Env
    
    style(service): revert EnvContent to Env
    
    style(container): revert EnvContent to Env
    
    refactor(env-vars): support default value
    
    refactor(service): use new env var component
    
    refactor(env-var): use one way data flow
    
    refactor(containers): remove unused function

--- app/portainer/views/stacks/edit/stackController.js
+++ app/portainer/views/stacks/edit/stackController.js
@@ -55,6 +55,7 @@ angular.module('portainer.app').controller('StackController', [
     $scope.formValues = {
       Prune: false,
       Endpoint: null,
+      Env: [],
     };
 
     $window.onbeforeunload = () => {
@@ -63,9 +64,14 @@ angular.module('portainer.app').controller('StackController', [
       }
     };
 
+    $scope.handleEnvVarChange = handleEnvVarChange;
+    function handleEnvVarChange(value) {
+      $scope.formValues.Env = value;
+    }
+
     $scope.duplicateStack = function duplicateStack(name, endpointId) {
       var stack = $scope.stack;
-      var env = FormHelper.removeInvalidEnvVars(stack.Env);
+      var env = FormHelper.removeInvalidEnvVars($scope.formValues.Env);
       EndpointProvider.setEndpointID(endpointId);
 
       return StackService.duplicateStack(name, $scope.stackFileContent, env, endpointId, stack.Type).then(onDuplicationSuccess).catch(notifyOnError);
@@ -164,7 +170,7 @@ angular.module('portainer.app').controller('StackController', [
 
     $scope.deployStack = function () {
       var stackFile = $scope.stackFileContent;
-      var env = FormHelper.removeInvalidEnvVars($scope.stack.Env);
+      var env = FormHelper.removeInvalidEnvVars($scope.formValues.Env);
       var prune = $scope.formValues.Prune;
       var stack = $scope.stack;
 
@@ -190,14 +196,6 @@ angular.module('portainer.app').controller('StackController', [
         .finally(function final() {
           $scope.state.actionInProgress = false;
         });
-    };
-
-    $scope.addEnvironmentVariable = function () {
-      $scope.stack.Env.push({ name: '', value: '' });
-    };
-
-    $scope.removeEnvironmentVariable = function (index) {
-      $scope.stack.Env.splice(index, 1);
     };
 
     $scope.editorUpdate = function (cm) {
@@ -269,6 +267,8 @@ angular.module('portainer.app').controller('StackController', [
           $scope.groups = data.groups;
           $scope.stack = stack;
           $scope.containerNames = ContainerHelper.getContainerNames(data.containers);
+
+          $scope.formValues.Env = $scope.stack.Env;
 
           let resourcesPromise = Promise.resolve({});
           if (stack.Status === 1) {
